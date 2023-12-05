c8b6f1e5ab13704f7e3284f5d47d5852df457919
kielkow <matheuskiel@fiorifer.com.br>
2023-12-05 10:40:19 -0300

    feat: prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
+import { User } from '@prisma/client'
 import { Controller, Get } from '@nestjs/common'
+
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/users')
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany()
   }
 }
