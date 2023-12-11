e88bf72cf48c362ac39d7d36337027ad556c9b10
kielkow <matheuskiel@fiorifer.com.br>
2023-12-11 20:08:52 -0300

    feat: check user credentials to generate access token

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma/prisma.service'
 
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccoutController } from './controllers/create-account.controller'
 
 @Module({
