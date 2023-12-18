8abcbec92710efa0d946062190f4ecb361c87118
kielkow <matheuskiel@fiorifer.com.br>
2023-12-18 10:37:15 -0300

    test: authenticate controller

--- src/controllers/authenticate/test.e2e-spec.ts
+++ src/controllers/authenticate/test.e2e-spec.ts
@@ -1,3 +1,29 @@
-test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(2)
+import request from 'supertest'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { AppModule } from '@/app.module'
+
+describe('Authenticate Controller (E2E)', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    await app.init()
+  })
+
+  it('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'jonhdoe@email.com',
+      password: '12345678',
+    })
+
+    expect(response.status).toBe(201)
+  })
 })
