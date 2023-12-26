05fa60a935dc65381d619ba763a9c2155fd69fec
kielkow <matheuskiel@fiorifer.com.br>
2023-12-26 12:03:13 -0300

    feat: prisma answers repository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,4 +1,4 @@
-import { Question as PrismaQuestion } from '@prisma/client'
+import { Prisma, Question as PrismaQuestion } from '@prisma/client'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
@@ -6,7 +6,7 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
 export class PrismaQuestionMapper {
-  static toDomain(prismaQuestion: PrismaQuestion) {
+  static toDomain(prismaQuestion: PrismaQuestion): Question {
     const {
       id,
       title,
@@ -40,14 +40,18 @@ export class PrismaQuestionMapper {
     )
   }
 
-  static toPersistence(question: Question) {
+  static toPersistence(
+    question: Question,
+  ): Prisma.QuestionUncheckedCreateInput {
     return {
       id: question.id,
       title: question.title,
       content: question.content,
       authorId: question.authorId.id,
-      bestAnswerId: question.bestAnswerId?.id,
+      bestAnswerId: question.bestAnswerId ? question.bestAnswerId.id : null,
       slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
     }
   }
 }
