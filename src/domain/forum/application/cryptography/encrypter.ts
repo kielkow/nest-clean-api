fe20d52cd61030988c93f2bff218695b9b3b301a
kielkow <matheuskiel@fiorifer.com.br>
2024-01-05 16:32:15 -0300

    wip: hasher class

--- src/domain/forum/application/cryptography/encrypter.ts
+++ src/domain/forum/application/cryptography/encrypter.ts
@@ -2,6 +2,4 @@ export abstract class Encrypter {
   abstract encrypt(value: string): Promise<string>
 
   abstract decrypt(value: string): Promise<string>
-
-  abstract compare(value: string, hash: string): Promise<boolean>
 }
